f341da03b32f0334ea449c86ab6e8d13f2d8b3d3
saghary <saghary@vanderbilt.edu>
2014-10-22 14:53:34 -0500

    changed pom

--- src/main/java/edu/vanderbilt/mis/test/HelloWorld.java
+++ src/main/java/edu/vanderbilt/mis/test/HelloWorld.java
@@ -1,6 +1,15 @@
 package edu.vanderbilt.mis.test;
 
+import org.apache.maven.shared.release.exec.MavenExecutorException;
+
+import com.atlassian.maven.plugins.jgitflow.helper.DefaultMavenExecutionHelper;
+
 
 public class HelloWorld {
       private final static String NEW_FIELD = "New Field";
+      
+      public void test() throws MavenExecutorException {
+         DefaultMavenExecutionHelper helper = new DefaultMavenExecutionHelper();
+         helper.execute( null, null );
+      }
 }
